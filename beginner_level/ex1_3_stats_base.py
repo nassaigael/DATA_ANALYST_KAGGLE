59cd8d8263a594499af828e1fabbc5817e4d798b
nassaigael <hei.nassai@gmail.com>
2026-06-18 12:04:42 +0300

    feat(stats): enhance print_stats function
    * Update print_stats to accept a file path and load data using
      load_data function.
    * Add calculations for average discount percentage, average rating,
      cheapest product, and least expensive product.
    * Return multiple statistics for better insights.
    * Adjusted imports for clarity and organization.

--- beginner_level/ex1_3_stats_base.py
+++ beginner_level/ex1_3_stats_base.py
@@ -1,15 +1,22 @@
 import os
 import sys
 
+from common_utils import cleaning_numeric_columns, load_data
+
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
-def print_stats(filename):
 
-	from common_utils import cleaning_numeric_columns
-	df = cleaning_numeric_columns(filename)
-	price_average_after_discount = df["discounted_price"].mean().round(2)
+def print_stats(path):
+	df = load_data(path)
+	df = cleaning_numeric_columns(df)
+	price_average_after_discount = df["discounted_price"].mean().__round__(2)
+	average_discount_percentage = df["discount_percentage"].mean().__round__(2)
+	average_rating = df["rating"].mean().__round__(2)
+	cheapest_product = df.loc[df["discounted_price"].idxmax(), "discounted_price"]
+	less_product = df.loc[df["discounted_price"].idxmin(), "discounted_price"]
 
-	return price_average_after_discount
+	return price_average_after_discount, average_discount_percentage, average_rating, cheapest_product, less_product
 
-file_to_test = "E:/DATA_ANALYST_KAGGLE/data/DATA_01.csv"
-print(print_stats(file_to_test))
+# file_to_test = "E:/DATA_ANALYST_KAGGLE/data/DATA_01.csv"
+# a, b, c, d, e = print_stats(file_to_test)
+# print(f"a = {a}$, b = {b}$, c = {c}, d = {d}$, e = {e}$")
